2d16c736ffcd45eb0b92a5e7674555d659d74cc9
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-10-16 07:18:17 +0530

    Completed Product Cover

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
     return (
         <nav className="fixed top-0 z-50 backdrop-blur-lg rounded-b-2xl w-full flex justify-between items-center h-20 px-4 lg:px-10">
             <div className="">
-                <h1 className="font-bold text-2xl">theBootStore</h1>
+                <h1 className="text-2xl uppercase font-extrabold">theBootStore.</h1>
             </div>
             <div className="flex">
-                <h1 className="mx-2">Login</h1>
-                <h1>More</h1>
+                <h1 className="mx-2 text-xl font-bold">Login</h1>
+                <h1 className="mx-2 text-xl font-bold">More</h1>
             </div>
         </nav>
     )
